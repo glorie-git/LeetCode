ef6131ac1c99bc4a75d0aef48cc4f5925920055b
Glorie Ramazani <glorieramazani@gmail.com>
2024-10-25 13:39:18 -0700

    Add solution.cpp

---
+++ solution.cpp
@@ -0,0 +1,20 @@
+int solution(vector<int>& A, int X) {
+    int N = A.size();
+    if (N == 0) {
+        return -1;
+    }
+    int l = 0;
+    int r = N - 1;
+    while (l < r) {
+        int m = (l + r) / 2;
+        if (A[m] > X) {
+            r = m - 1;
+        } else {
+            l = m;
+        }
+    }
+    if (A[l] == X) {
+        return l;
+    }
+    return -1;
+}
