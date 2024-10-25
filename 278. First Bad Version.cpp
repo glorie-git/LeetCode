22e47bc6498ada568047463f939e570a233e3159
Glorie Ramazani <glorieramazani@gmail.com>
2024-10-25 13:30:13 -0700

    Updated 278. First Bad Version.cpp

--- 278. First Bad Version.cpp
+++ 278. First Bad Version.cpp
@@ -4,11 +4,17 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        for(int i = 1; i <= n; i++){
-            if (isBadVersion(i)){
-                return i;
+        int left = 1, right = n;
+
+        while(left < right){
+            int mid = left + (right - left)/2;
+            
+            if(isBadVersion(mid)){
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return -1;
+        return left;
     }
 };
